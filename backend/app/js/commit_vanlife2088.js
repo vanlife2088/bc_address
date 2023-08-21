ea966dacabbb0cdb57cb7e3d3d1b9d69dabd55f6
Shenzhen Yao <junk.them@hotmail.com>
2023-08-20 22:17:44 -0700

    20230820b

--- backend/app/js/commit_vanlife2088.js
+++
@@ -1,3 +0,0 @@
-// commit to vanlife 2088 via auth code
-// https://stateful.com/blog/github-api-nodejs
-// https://stackoverflow.com/questions/62177166/how-to-do-git-commit-using-personal-access-token
